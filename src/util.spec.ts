f4d1182d8a89ba522cc79ca651b44495549270de
Tim Otto <tim@timotto.io>
2018-02-11 12:45:45 +0100

    remove dead code, adjust coverage

--- src/util.spec.ts
+++ src/util.spec.ts
@@ -1,155 +1,36 @@
-import {PretryOptions, Util} from "./util";
+import {Util} from "./util";
 
 describe('Class Util', () => {
-    describe('Function: pretry', () => {
+    describe('envOrDefault', () => {
+        const testKeys = ['TEST_ENV1', 'TEST_ENV2'];
+        const cleanup = () => testKeys.forEach(key => delete process.env[key]);
+        beforeEach(cleanup);
+        afterEach(cleanup);
+        it('returns the value of the environment variable named "envKey"', () => {
+            const testKey = testKeys[0];
+            const expectedValue = 'expected value';
+            const unexpectedValue = 'unexpected value';
 
-        it('executes the given promiseFunction', done => {
-            const doneFunction = () => {
-                expect(promiseExecuted).toBeTruthy();
-                done();
-            };
+            // given
+            process.env[testKey] = expectedValue;
 
-            let promiseExecuted = false;
-            Util.pretry(() => new Promise(resolve => {
-                promiseExecuted = true;
-                resolve();
-            }), {times: 1})
-                .then(doneFunction)
-                .catch(doneFunction);
+            // when
+            const actualResult = Util.envOrDefault(testKey, unexpectedValue);
+
+            // then
+            expect(actualResult).toEqual(expectedValue);
         });
+        it('returns the defaultValue if the value of the environment variable named "envKey" is not defined', () => {
+            const testKey = testKeys[0];
+            const expectedValue = 'expected value';
 
-        it('executes the given promiseFunction as long as it rejects the promise', done => {
-            const resolveAfterCall = 5;
-            const doneFunction = () => {
-                expect(promiseExecutions).toBe(5);
-                done();
-            };
+            // given
 
-            let promiseExecutions = 0;
+            // when
+            const actualResult = Util.envOrDefault(testKey, expectedValue);
 
-            Util.pretry(() => new Promise((resolve, reject) => {
-                promiseExecutions++;
-                if (promiseExecutions < resolveAfterCall) return reject(false);
-                else resolve(true);
-            }), {times: 10})
-                .then(doneFunction)
-                .catch(doneFunction);
-        });
-
-        it('throws the error if the promiseFunction rejects as often as the "times" property value', done => {
-            const options = {times: 2};
-            const expectedError = 'this is expected';
-            let actualExecutions = 0;
-
-            Util.pretry(() => {
-                actualExecutions++;
-                return Promise.reject(expectedError);
-            }, options)
-                .then(() => {
-                    expect(undefined).toBe(expectedError);
-                    done();
-                })
-                .catch(actualError => {
-                    expect(actualExecutions).toBe(options.times);
-                    expect(actualError).toBe(expectedError);
-                    done();
-                })
-        });
-
-        it('does not wait before retrying by default', done => {
-            const options: PretryOptions = {times: 2};
-            let timeOfFirstAttempt = undefined;
-            let timeOfSecondAttempt = undefined;
-
-            let actualExecutions = 0;
-
-            Util.pretry(() => {
-                actualExecutions++;
-                switch (actualExecutions) {
-                    case 1:
-                        timeOfFirstAttempt = new Date().getTime();
-                        return Promise.reject('nope');
-                    default:
-                        timeOfSecondAttempt = new Date().getTime();
-                        return Promise.resolve('ok');
-                }
-            }, options)
-                .then(() => {
-                    expect(timeOfSecondAttempt/1000).toBeCloseTo(timeOfFirstAttempt/1000, 1);
-                    done();
-                })
-        });
-
-        it('waits before retrying if specified by the "wait" property', done => {
-            const options: PretryOptions = {times: 2, wait: 500};
-            let timeOfFirstAttempt = undefined;
-            let timeOfSecondAttempt = undefined;
-
-            let actualExecutions = 0;
-
-            Util.pretry(() => {
-                actualExecutions++;
-                switch (actualExecutions) {
-                    case 1:
-                        timeOfFirstAttempt = new Date().getTime();
-                        return Promise.reject('nope');
-                    default:
-                        timeOfSecondAttempt = new Date().getTime();
-                        return Promise.resolve('ok');
-                }
-            }, options)
-                .then(() => {
-                    expect((timeOfSecondAttempt-timeOfFirstAttempt)/1000).toBeCloseTo(options.wait/1000, 1);
-                    done();
-                })
-        });
-    });
-
-    describe('Function: pserial', () => {
-        it('executes all the promises', done => {
-            const promiseMakers: RecordingPromiseFunctionMaker[] = [
-                new RecordingPromiseFunctionMaker(),
-                new RecordingPromiseFunctionMaker(),
-                new RecordingPromiseFunctionMaker()
-            ];
-            const promiseFunctions = promiseMakers.map(pm => pm.makePromiseMakerFunction());
-
-            Util.pserial(promiseFunctions)
-                .then(() => {
-                    promiseMakers.forEach(fn => expect(fn.called).toBeTruthy());
-                    done();
-                });
-        });
-
-        it('stops the execution with the first rejection', done => {
-            const expected = new RecordingPromiseFunctionMaker();
-            const unexpected = new RecordingPromiseFunctionMaker();
-            const promiseFunctions = [
-                expected.makePromiseMakerFunction(),
-                rejectingPromiseFunction,
-                unexpected.makePromiseMakerFunction()
-            ];
-
-            Util.pserial(promiseFunctions)
-                .catch(() => {
-                    expect(expected.called).toBeTruthy();
-                    expect(unexpected.called).toBeFalsy();
-                    done();
-                })
+            // then
+            expect(actualResult).toEqual(expectedValue);
         });
     });
 });
-
-class RecordingPromiseFunctionMaker {
-    public called: boolean = false;
-    public makePromiseMakerFunction(): (() => Promise<any>) {
-        return () => {
-            return new Promise(resolve => {
-                this.called = true;
-                resolve();
-            });
-        }
-    }
-}
-
-const rejectingPromiseFunction = () => Promise.reject(false);
--- src/util.ts
+++ src/util.ts
@@ -14,48 +14,4 @@ export class Util {
 
         return value;
     }
-
-    public static pretry(promiseFunction, options: PretryOptions): Promise<any> {
-        return new Promise(((resolve, reject) => {
-            let attempts = 0;
-
-            const resolution = result => resolve(result);
-
-            const attempt = () => {
-                attempts++;
-                return promiseFunction()
-                    .then(resolution)
-                    .catch(retry);
-            };
-
-            const retry = error => {
-                if (attempts >= options.times) {
-                    return reject(error);
-                }
-                if (options.wait) {
-                    return new Promise(resolve => setTimeout(() => resolve(
-                        attempt()),
-                        options.wait));
-                }
-                return attempt();
-            };
-
-            return attempt();
-        }));
-    }
-
-    public static pserial<T>(promiseMakerFunctions: ((T) => Promise<T>)[], initialValue?: Promise<T>): Promise<T> {
-        const _initialValue = initialValue !== undefined
-            ? initialValue
-            : Promise.resolve(undefined);
-
-        const chainFunction = (p: Promise<T>, fn: (T) => Promise<T>) => p.then(fn);
-
-        return promiseMakerFunctions.reduce(chainFunction, _initialValue);
-    }
-}
-
-export class PretryOptions {
-    times?: number = 5;
-    wait?: number = 0;
 }
